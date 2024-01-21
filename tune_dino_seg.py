65257d1185943fd9cda487d770294825c4097121
Sajtospoga01 <abodrogai@gmail.com>
2024-01-21 19:15:10 +0000

    Update samples_per_gpu to 32 in build_dataloader function

--- tune_dino_seg.py
+++ tune_dino_seg.py
@@ -177,7 +177,7 @@ def main():
     data_loaders = [
         build_dataloader(
             ds,
-            samples_per_gpu = 1,
+            samples_per_gpu = 32,
             workers_per_gpu = 1,
             seed = 42,
             drop_last = True,

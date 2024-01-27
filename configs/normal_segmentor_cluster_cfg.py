7530ab7b50796382a08b2ded9c75ad264773d08e
Sajtospoga01 <abodrogai@gmail.com>
2024-01-27 15:17:26 +0000

    Update FocalLoss configuration to use sigmoid

--- configs/normal_segmentor_cluster_cfg.py
+++ configs/normal_segmentor_cluster_cfg.py
@@ -210,7 +210,7 @@ model = dict(
         norm_cfg=dict(type='SyncBN', requires_grad=True),
         align_corners=False,
         loss_decode=dict(
-            type='FocalLoss', use_sigmoid=False, loss_weight=1.0)
+            type='FocalLoss', use_sigmoid=True, loss_weight=1.0)
         
         ),
     test_cfg=dict(mode='slide', crop_size=(64, 64), stride=(32, 32)))

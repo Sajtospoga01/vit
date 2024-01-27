fa002d57fa93b3c32a006ca6f70270c8ee8554d8
Sajtospoga01 <abodrogai@gmail.com>
2024-01-27 14:31:11 +0000

    Update decoder head to use TransformerDecoder

--- src/dinov2/models/decoder.py
+++ src/dinov2/models/decoder.py
@@ -3,7 +3,8 @@ from torch import nn
 from mmseg.models.builder import HEADS
 from mmseg.models.decode_heads.decode_head import BaseDecodeHead
 from mmseg.ops import resize
-
+from src.dinov2.layers import Mlp, PatchEmbed,SwiGLUFFNFused, MemEffAttention, NestedTensorBlock as Block
+from src.vit_model.custom_layers import get_2d_sincos_pos_embed
 @HEADS.register_module()
 class MultiScaleDecoder(BaseDecodeHead):
 
@@ -94,4 +95,125 @@ class MultiScaleDecoder(BaseDecodeHead):
         """Forward function."""
         output = self._forward_feature(inputs)
         output = self.cls_seg(output)
+        return output
+    
+
+
+@HEADS.register_module()
+class TransformerDecoder(BaseDecodeHead):
+
+    def __init__(self,img_size,embed_dim,decoder_embed_dim,patch_size,decoder_depth,classes,num_heads, resize_factors = None, **kwargs):
+        super().__init__(**kwargs)
+        self.embed_dim = embed_dim
+        self.patch_size = patch_size
+        self.num_patches = (img_size[0] // patch_size) *  (img_size[1] // patch_size)
+        self.decoder_embed = nn.Linear(embed_dim, decoder_embed_dim, bias=True)
+        self.decoder_pos_embed = nn.Parameter(torch.zeros(1, self.num_patches + 1, decoder_embed_dim), requires_grad=False)  # fixed sin-cos embedding
+        self.decoder_blocks = nn.ModuleList([
+            Block(decoder_embed_dim,num_heads = num_heads, attn_class=MemEffAttention)
+            for i in range(decoder_depth)])
+        
+        self.decoder_norm = nn.LayerNorm(decoder_embed_dim)
+        self.decoder_pred = nn.Linear(decoder_embed_dim, patch_size**2 * classes, bias=True) # decoder to patch
+        self.classes = classes
+        self.cls_token = nn.Parameter(torch.randn(1, 1, embed_dim))
+        self.initialize_weights()
+
+    def initialize_weights(self):
+        # initialization
+        # initialize (and freeze) pos_embed by sin-cos embedding
+       
+      
+
+        decoder_pos_embed = get_2d_sincos_pos_embed(self.decoder_pos_embed.shape[-1], int(self.num_patches**.5), cls_token=True)
+        self.decoder_pos_embed.data.copy_(torch.from_numpy(decoder_pos_embed).float().unsqueeze(0))
+
+        # timm's trunc_normal_(std=.02) is effectively normal_(std=0.02) as cutoff is too big (2.)
+        # torch.nn.init.normal_(self.cls_token, std=.02)
+        # torch.nn.init.normal_(self.mask_token, std=.02)
+
+        # initialize nn.Linear and nn.LayerNorm
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            # we use xavier_uniform following official JAX ViT:
+            torch.nn.init.xavier_uniform_(m.weight)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+
+    def _forward_feature(self, inputs):
+        """Forward function for feature maps before classifying each pixel with
+        ``self.cls_seg`` fc.
+
+        Args:
+            inputs (list[Tensor]): List of multi-level img features.
+
+        Returns:
+            feats (Tensor): A tensor of shape (batch_size, self.channels,
+                H, W) which is feature map for last layer of decoder head.
+        """
+        # print("inputs", [i.shape for i in inputs])
+        x = self._forward_decode(inputs)
+
+        # print("feats", feats.shape)
+        return x
+    
+    def unpatchify(self, x):
+        """
+        x: (N, L, patch_size**2 *3)
+        imgs: (N, 3, H, W)
+        """
+        p = self.patch_size
+        h = w = int(x.shape[1]**.5)
+        assert h * w == x.shape[1]
+
+        x = x.reshape(shape=(x.shape[0], h, w, p, p, self.classes))
+        x = torch.einsum('nhwpqc->nchpwq', x)
+        imgs = x.reshape(shape=(x.shape[0], self.classes, h * p, h * p))
+        return imgs
+
+    def _forward_decode(self, inputs):
+        """Transform inputs for decoder.
+        Args:
+            inputs (list[Tensor]): List of multi-level img features.
+        Returns:
+            Tensor: The transformed inputs
+        """
+        print("inputs", [i.shape for i in inputs])
+        
+        inputs = torch.cat(inputs, dim=1)
+        inputs = inputs.view(-1,self.embed_dim,self.num_patches)
+        inputs = inputs.transpose(1, 2)
+        batch_size = inputs.shape[0]
+        cls_tokens = self.cls_token.expand(batch_size, -1, -1)  # Expand CLS token to match the batch size
+        inputs = torch.cat([cls_tokens, inputs], dim=1)
+        print("inputs shape: ",inputs.shape)
+        x = self.decoder_embed(inputs)
+
+        # add pos embed
+        x = x + self.decoder_pos_embed
+
+        # apply Transformer blocks
+        for blk in self.decoder_blocks:
+            x = blk(x)
+        x = self.decoder_norm(x)
+
+        # predictor projection
+        x = self.decoder_pred(x)
+
+        # remove cls token
+        x = x[:, 1:, :]
+        x = self.unpatchify(x)
+
+        return x
+
+    def forward(self, inputs):
+        """Forward function."""
+        output = self._forward_feature(inputs)
+        # output = self.cls_seg(output)
+        print(output.shape)
         return output

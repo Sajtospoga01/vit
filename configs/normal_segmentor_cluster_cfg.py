b1dea7be940a7fb9a5756efb26d83a1e53906d1c
Sajtospoga01 <abodrogai@gmail.com>
2024-01-28 16:31:43 +0000

    Update loss function to DiceLoss in model configuration

--- configs/normal_segmentor_cluster_cfg.py
+++ configs/normal_segmentor_cluster_cfg.py
@@ -213,7 +213,7 @@ model = dict(
         norm_cfg=dict(type='SyncBN', requires_grad=True),
         align_corners=False,
         loss_decode=dict(
-            type='FocalLoss', loss_weight=1.0)
+            type='DiceLoss', loss_weight=1.0)
         
         ),
     test_cfg=dict(mode='slide', crop_size=(64, 64), stride=(32, 32)))

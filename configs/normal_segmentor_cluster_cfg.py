3e34f90972f18667c84c875293fd713e8d4681c5
Sajtospoga01 <abodrogai@gmail.com>
2024-03-02 23:25:53 +0000

    Update FocalLoss parameters in model configuration

--- configs/normal_segmentor_cluster_cfg.py
+++ configs/normal_segmentor_cluster_cfg.py
@@ -230,7 +230,7 @@ model = dict(
         align_corners=False,
         loss_decode=dict(
             # type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
-            type='FocalLoss', gamma=2, alpha=0.55, loss_weight=1.0)),
+            type='FocalLoss', gamma=2.0, alpha=0.55, loss_weight=1.0)),
 
     test_cfg=dict(mode='slide', crop_size=(64, 64), stride=(32, 32)))
 auto_resume = True
